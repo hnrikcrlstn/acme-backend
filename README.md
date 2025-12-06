a0f167888a01598356e81b31c47f2a9e2785a433
hc <hc@localhost.localdomain>
2025-12-06 21:50:44 +0100

    Base modul added

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # acme-backend
+
+### Installation
+Add module from the private registry
+Call the module with
+```hcl
+module "demo-module" {
+  for_each = var.workspaces_to_deploy
+  source              = "app.terraform.io/hcarlstein/demo-module/tfe"
+  version             = "1.0.0"
+  github_organization = each.value.github_organization
+  tfe_organization    = each.value.tfe_organization
+  workspace_settings = {
+    name = each.value.workspace_settings.name
+    auto_destroy_activity_duration = try(each.value.workspace_settings.auto_destroy_activity_duration, null)
+    vcs_repo = {
+      branch     = each.value.workspace_settings.vcs_repo.branch
+      identifier = each.value.workspace_settings.vcs_repo.identifier
+    }
+  }
+}
+```
---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.71.0"
+    }
+  }
+}
+
+data "tfe_github_app_installation" "this" {
+  name = var.github_organization
+}
+
+resource "tfe_workspace" "this" {
+  name = var.workspace_settings.name
+  organization = var.tfe_organization
+  auto_destroy_activity_duration = var.workspace_settings.auto_destroy_activity_duration
+  vcs_repo {
+    branch                     = var.workspace_settings.vcs_repo.branch
+    identifier                 = var.workspace_settings.vcs_repo.identifier
+    github_app_installation_id = data.tfe_github_app_installation.this.id
+  }
+}
+variable "github_organization" {
+  type = string
+}
+
+variable "tfe_organization" {
+  type = string
+}
+
+variable "workspace_settings" {
+  type = object({
+    name = string
+    auto_destroy_activity_duration = optional(string)
+    vcs_repo = object({
+      branch     = string
+      identifier = string
+    })
+  })
+}
